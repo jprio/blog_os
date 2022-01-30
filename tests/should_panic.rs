4d594cc79a1ce9bcce997c30cbd4e8b61084600e
jprio <jeanpierre.rio@gmail.com>
2022-01-30 07:30:26 +0100

    First commit

---
+++ tests/should_panic.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+use blog_os::{exit_qemu, serial_print, serial_println, QemuExitCode};
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    should_fail();
+
+    loop {}
+}
+fn should_fail() {
+    serial_print!("should_panic::should_fail...\t");
+    assert_eq!(0, 1);
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
